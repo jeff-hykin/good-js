d2f7d98a0c842b44ab71077bc29626a5f4cab5e3
Jeff Hykin <jeff.hykin@gmail.com>
2022-03-22 20:30:45 -0500

    add permutations

--- source/itertools.js
+++ source/itertools.js
@@ -1,4 +1,3 @@
-
 /**
  * wrapAroundGet
  *
@@ -10,9 +9,9 @@
  *     wrapAroundGet(3,items) // 9
  *     wrapAroundGet(4,items) // 5
  */
-export const wrapAroundGet = (number, list) => list[(number % list.length + list.length) % list.length]
+export const wrapAroundGet = (number, list) => list[((number % list.length) + list.length) % list.length]
 
-export const makeIterable = function*(object) {
+export const makeIterable = function* (object) {
     if (object instanceof Map) {
         yield* object.entries()
     }
@@ -20,27 +19,27 @@ export const makeIterable = function*(object) {
         // Array, Set, string, custom classes, etc
         if (object[Symbol.iterator] instanceof Function) {
             yield* object
-        // pure objects
+            // pure objects
         } else {
             yield* Object.entries(object)
         }
     }
 }
 
-export const reverse = function(object) {
+export const reverse = function (object) {
     // make a copy, then reverse it
     return [...makeIterable(object)].reverse()
 }
 
-export const zipIter = function*(...iterables) {
-    iterables = iterables.map(each=>makeIterable(each))
+export const zipIter = function* (...iterables) {
+    iterables = iterables.map((each) => makeIterable(each))
     while (true) {
-        const nexts = iterables.map(each=>each.next())
+        const nexts = iterables.map((each) => each.next())
         // if all are done then stop
-        if (nexts.every(each=>each.done)) {
+        if (nexts.every((each) => each.done)) {
             break
         }
-        yield nexts.map(each=>each.value) 
+        yield nexts.map((each) => each.value)
     }
 }
 
@@ -53,7 +52,7 @@ export const zipIter = function*(...iterables) {
  *     zip([1,2,3],[1,2])
  *     // [  [1,1], [2,2], [3,undefined]  ]
  */
-export const zip = function(...iterables) {
+export const zip = function (...iterables) {
     return [...zipIter(...iterables)]
 }
 
@@ -65,7 +64,7 @@ export const zip = function(...iterables) {
  * @example
  *     countIter({start: 0, end: array.length-1, step: 1})
  */
-export const countIter = function*({start=0, end=Infinity, step=1}) {
+export const countIter = function* ({ start = 0, end = Infinity, step = 1 }) {
     let count = start
     while (count <= end) {
         yield count
@@ -81,8 +80,8 @@ export const countIter = function*({start=0, end=Infinity, step=1}) {
  * @example
  *     count({start: 0, end: array.length-1, step: 1})
  */
-export const count = function({start=0, end=Infinity, step=1}) {
-    return [...countIter({start, end, step})]
+export const count = function ({ start = 0, end = Infinity, step = 1 }) {
+    return [...countIter({ start, end, step })]
 }
 
 /**
@@ -94,7 +93,7 @@ export const count = function({start=0, end=Infinity, step=1}) {
  *     enumerateIter(['a','b'], ['A', 'B'])
  *     // [  [0,'a','A'],  [1,'b','B']  ]
  */
-export const enumerateIter = function*(...iterables) {
+export const enumerateIter = function* (...iterables) {
     let index = 0
     for (const each of zipIter(...iterables)) {
         yield [index, ...each]
@@ -110,8 +109,48 @@ export const enumerateIter = function*(...iterables) {
  *     enumerate(['a','b'], ['A', 'B'])
  *     // [  [0,'a','A'],  [1,'b','B']  ]
  */
-export const enumerate = function(...iterables) {
+export const enumerate = function (...iterables) {
     return [...enumerateIter(...iterables)]
 }
 
 // TODO: flatIterator
+
+/**
+ * Permutations
+ *
+ * @example
+ *     [...permuteIter([1,2,3])]
+ *     // [[1,2,3],[2,1,3],[3,1,2],[1,3,2],[2,3,1],[3,2,1]]
+ */
+export const permuteIter = function*(elements) {
+    yield elements.slice()
+    const length = elements.length
+    const c = new Array(length).fill(0)
+    let i = 1, k, p
+
+    while (i < length) {
+        if (c[i] < i) {
+            k = i % 2 && c[i]
+            p = elements[i]
+            elements[i] = elements[k]
+            elements[k] = p
+            ++c[i]
+            i = 1
+            yield elements.slice()
+        } else {
+            c[i] = 0
+            ++i
+        }
+    }
+}
+
+/**
+ * Permutations
+ *
+ * @example
+ *     permute([1,2,3])
+ *     // [[1,2,3],[2,1,3],[3,1,2],[1,3,2],[2,3,1],[3,2,1]]
+ */
+export const permute = function(elements) {
+    [...permuteIter(elements)]
+}
--- tests/itertools.js
+++ tests/itertools.js
@@ -1,5 +1,7 @@
-import {zip, enumerate, count, wrapAroundGet} from "../source/itertools.js"
+import {zip, enumerate, count, wrapAroundGet, permuteIter } from "../source/itertools.js"
 
 var a,b,c,d
 
-console.log(enumerate(['a','b'], ['A', 'B']))
+console.log(JSON.stringify(enumerate(['a','b'], ['A', 'B'])))
+console.log(JSON.stringify(zip(['a','b'], ['A', 'B'])))
+console.log(`permuteIter([1,2,3])`, JSON.stringify([...permuteIter([1,2,3])]))
