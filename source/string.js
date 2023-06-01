0125451ff8c19c52b48555e79f6030233be00ce3
Jeff Hykin <jeff.hykin@gmail.com>
2023-06-01 08:46:07 -0500

    fix findAll for 0-length matches

--- source/string.js
+++ source/string.js
@@ -177,15 +177,32 @@ export const toScreamingtoSnakeCase = (str) => {
 }
 
 export const findAll = (regexPattern, sourceString) => {
-    const output = []
-    let match
-    // make sure the pattern has the global flag
-    const regexPatternWithGlobal = RegExp(regexPattern, [...new Set("g" + regexPattern.flags)].join(""))
-    while ((match = regexPatternWithGlobal.exec(sourceString))) {
-        // get rid of the string copy
-        delete match.input
+    var output = []
+    var match
+    // auto-add global flag while keeping others as-is
+    var regexPatternWithGlobal = regexPattern.global ? regexPattern : RegExp(regexPattern, regexPattern.flags+"g")
+    while (match = regexPatternWithGlobal.exec(sourceString)) {
         // store the match data
         output.push(match)
+        // zero-length matches will end up in an infinite loop, so increment by one char after a zero-length match is found
+        if (match[0].length == 0) {
+            regexPatternWithGlobal.lastIndex += 1
+        }
+    }
+    return output
+}
+
+export function* iterativelyFindAll(regexPattern, sourceString) {
+    var match
+    // auto-add global flag while keeping others as-is
+    const regexPatternWithGlobal = regexPattern.global ? regexPattern : RegExp(regexPattern, regexPattern.flags+"g")
+    while (match = regexPatternWithGlobal.exec(sourceString)) {
+        // store the match data
+        yield match
+        // zero-length matches will end up in an infinite loop, so increment by one char after a zero-length match is found
+        if (match[0].length == 0) {
+            regexPatternWithGlobal.lastIndex += 1
+        }
     }
     return output
 }
