05c0d3802009652c3c4827ca44a049f9f7e9e97f
Jeff Hykin <jeff.hykin@gmail.com>
2023-06-16 11:19:10 -0500

    add regex interpolater

--- source/string.js
+++ source/string.js
@@ -1,4 +1,4 @@
-
+import { zip } from "./iterable.js"
 export const capitalize = (string) => string.replace(/\b\w/g, (chr) => chr.toUpperCase())
 
 /**
@@ -263,6 +263,79 @@ export function escapeRegexMatch(string) {
 export function escapeRegexReplace(string) {
     return string.replace(/\$/g, '$$$$')
 }
+
+// 
+// regex`pattern${/stuff/}${`stuff`}`.i
+// 
+    // these are helpers for the .i part, which requires a proxy object
+    // declaring it out here saves on memory so there aren't a million instances of expensive proxy objects
+    let proxyRegExp
+    const regexProxyOptions = Object.freeze({
+        get(original, key) {
+            // if its flags, return a copy with those flags set
+            if (key.match(/^[igymu]+$/)) {
+                return proxyRegExp(original, key)
+            }
+            return original[key]
+        },
+        set(original, key, value) {
+            return original[key] = value
+        },
+    })
+    proxyRegExp = (parent, flags)=> {
+        const regex = new RegExp(parent, flags)
+        const output = new Proxy(regex, regexProxyOptions)
+        Object.setPrototypeOf(output, Object.getPrototypeOf(regex))
+        return output
+    }
+    // this is a helper to make regex() and regex.stripFlags() have the same underlying functionality
+    function regexWithStripWarning(shouldStrip) {
+        return (strings, ...values) => {
+            let newRegexString = ""
+            for (const [ string, value ] of zip(strings,values)) {
+                newRegexString += string
+                if (value instanceof RegExp) {
+                    // ignore value.global since its common and wouldn't really mean anything in this context
+                    if (!shouldStrip && (value.ignoreCase||value.sticky||value.multiline||value.unicode)) {
+                        console.warn(`Warning: flags inside of regex:\n    The RegExp trigging this warning is: ${value}\n    When calling the regex interpolater (e.g. regex\`something\${stuff}\`)\n    one of the \${} values (the one above) was a RegExp with a flag enabled\n    e.g. /stuff/i  <- i = ignoreCase flag enabled\n    When the /stuff/i gets interpolated, its going to loose its flags\n    (thats what I'm warning you about)\n    \n    To disable/ignore this warning do:\n        regex.stripFlags\`something\${/stuff/i}\`\n    If you want to add flags to the output of regex\`something\${stuff}\` do:\n        regex\`something\${stuff}\`.i   // ignoreCase\n        regex\`something\${stuff}\`.ig  // ignoreCase and global\n        regex\`something\${stuff}\`.gi  // functionally equivlent\n`)
+                    }
+                    // ex; `/blah/i` => `blah`
+                    const regexContent = `${value}`.slice(1,).replace(/\/.*$/,"")
+                    
+                    // the `(?: )` is a non-capture group to prevent alternation from becoming a problem
+                    // for example: `a|b` + `c|d` becoming `a|bc|d` (bad/incorrect) instead of becoming `(?:a|b)(?:c|d)` (correct)
+                    newRegexString += `(?:${regexContent})`
+                } else if (value != null) {
+                    newRegexString += escapeRegexMatch(toString(value))
+                }
+            }
+            // this exists to make regex``.i, regex``.gi, etc work
+            return proxyRegExp(newRegexString,"")
+        }
+    }
+    
+    /**
+     * interpolate strings/regex
+     *
+     * @example
+     *     const someName = "nameWithWeirdSymbols\\d(1)$@[]"
+     *     const versionPattern = /\d+\.\d+\.\d+/
+     *     const combined = regex`blah "${someName}"@${versionPattern}`.i
+     *     // the string is regex-escaped, but the regex is kept as-is:
+     *     /blah "nameWithWeirdSymbols\\d\(1\)\$@\[\]"@(?:\d+\.\d+)/i
+     * 
+     *     // NOTE: interpolating with flags will give a warning that they will be stripped:
+     *     const versionPattern2 = /\d+\.\d+\.\d+/iu
+     *     regex`blah thing@${versionPattern2}` // >>> warning the "iu" flags will be stripped
+     *     // use this to intentionally strip flags
+     *     regex.stripFlags`blah thing@${versionPattern2}` // no warning
+     * 
+     * @param arg1 - a template string
+     * @returns {RegExp} output
+     *
+     */
+    export const regex = regexWithStripWarning(false)
+    regex.stripFlags = regexWithStripWarning(true)
 
 // https://stackoverflow.com/questions/2460177/edit-distance-in-python, translated to JS
 export function levenshteinDistanceBetween(s1, s2) {
--- tests/string.js
+++ tests/string.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env -S deno run --allow-all
-import { capitalize, indent, toCamelCase, toPascalCase, toKebabCase, toSnakeCase, toScreamingtoKebabCase, toScreamingtoSnakeCase, levenshteinDistanceOrdering, levenshteinDistanceBetween } from "../source/string.js"
+import { capitalize, indent, toCamelCase, toPascalCase, toKebabCase, toSnakeCase, toScreamingtoKebabCase, toScreamingtoSnakeCase, levenshteinDistanceOrdering, levenshteinDistanceBetween, extractFirst, regex } from "../source/string.js"
 
 console.log(`${capitalize("howdy_howdy_howdy")}`)
 console.log(`${toCamelCase("howdy_howdy_howdy")}`)
@@ -7,3 +7,45 @@ console.log(`${toPascalCase("howdy_howdy_howdy")}`)
 console.log(`${indent({string:"howdy_howdy_howdy"})}`)
 console.debug(`levenshteinDistanceOrdering({word: "howdee", otherWords:[ "bob", "heydo", "how", "howdy", "gigem" ]}) is:`,levenshteinDistanceOrdering({word: "howdee", otherWords:[ "bob", "heydo", "how", "howdy", "gigem" ]}))
 console.debug(`levenshteinDistanceBetween("howdy", "howdee") is:`,levenshteinDistanceBetween("howdy", "howdee"))
+
+// basic example
+var remaining = "blah thing3: num8: 1"
+var { remaining, extraction: thing, } = extractFirst({ pattern: /thing\d: /, from: remaining })
+var { remaining, extraction: num,   } = extractFirst({ pattern: /num\d: /, from: remaining })
+// NOTE: the "blah" is still there. Use "^" e.g. /^thing/ to only extract from the front
+console.log(remaining === "blah 1") // true
+console.log(thing === "thing3: ") // true
+
+// full example
+var remaining = "blah thing3: num8: 1"
+var { preText, match, extraction, postText, remaining } = extractFirst({ pattern: /thing(\d): /, from: remaining })
+console.debug(`({ preText, match, extraction, postText, remaining }) is:`,({ preText, match, extraction, postText, remaining }))
+// preText == "blah "
+// match == [ index: 5, "thing5: ", "5" ] // usual regex match object
+// extraction == "thing5: "
+// postText == "num8: 1"
+// remaining == "blah num8: 1"
+
+var output = regex`howdy${"(parens)"}${/(captureGroup)/}`.i
+console.debug(`regex is:`,output)
+
+var someName = "nameWithWeirdSymbols\\d(1)$@[]"
+var somePattern = /\d+\.\d+/
+var combined = regex`blah ${someName} blah ${somePattern}`.i
+// the string is regex-escaped, but the regex is kept as-is
+console.log(combined) 
+
+var someName = "nameWithWeirdSymbols\\d(1)$@[]"
+var versionPattern = /\d+\.\d+\.\d+/
+var combined = regex`blah "${someName}"@${versionPattern}`.i
+// the string is regex-escaped, but the regex is kept as-is:
+console.debug(`combined is:`,combined)
+
+// NOTE: interpolating with flags will give a warning that they will be stripped:
+var versionPattern2 = /\d+\.\d+\.\d+/iu
+console.warn = console.log // to make it use stdout
+console.log("should show warning")
+console.debug(`regex\`blah thing@\${versionPattern}\` is:`,regex`blah thing@${versionPattern2}`)
+// use this to intentionally strip flags
+console.log("should not show warning")
+console.debug(`regex.stripFlags\`blah thing@\${versionPattern}\` is:`,regex.stripFlags`blah thing@${versionPattern2}`)
