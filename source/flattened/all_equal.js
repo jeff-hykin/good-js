62fbf2a1f1ebe09f7465969e4fc4282d18aae58f
Jeff Hykin <jeff.hykin@gmail.com>
2023-11-14 09:11:55 -0600

    add all_equal

---
+++ source/flattened/all_equal.js
@@ -0,0 +1,26 @@
+/**
+ * Fast All-Same-Element checker
+ *
+ * @example
+ * ```js
+ * allEqual([1,2,1,1,1]) // false
+ * allEqual([1,1,1,1,1]) // true
+ * ```
+ *
+ * @returns {boolean} output
+ *
+ */
+export function allEqual(anIterable) {
+    // looks weird but the outer for loop only inits the prev
+    // its done this way to both be generic (any iterator) and optimal (no if(firstElement) check or .length check)
+    for (let prev of anIterable) {
+        for (const each of anIterable) {
+            if (each!=prev) {
+                return false
+            }
+            prev = each
+        }
+        break
+    }
+    return true
+}
