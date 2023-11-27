08128b3a9263de28e41e8f962d27b68ff26effe0
Jeff Hykin <jeff.hykin@gmail.com>
2023-11-27 10:30:36 -0600

    add baseParseArgs

---
+++ source/flattened/base_parse_args.js
@@ -0,0 +1,36 @@
+export function baseParseArgs(args) {
+    const flags = {}
+    const namedArgs = {}
+    const numberedArgs = []
+    let doubleDashWasPassed = false
+    let argName = null
+    for (const each of args) {
+        if (argName != null) {
+            const name = argName
+            argName = null
+            // multiple entries of same argument become a list
+            if (Object.keys(namedArgs).includes(name)) {
+                namedArgs[name] = [ namedArgs[name], each ]
+            } else {
+                namedArgs[name] = each
+            }
+        }
+        if (!disableDoubleDash && each == '--') {
+            doubleDashWasPassed = true
+        }
+        if (doubleDashWasPassed) {
+            numberedArgs.push(each)
+            continue
+        }
+        if (each.startsWith("--")) {
+            argName = each.slice(2,)
+            continue
+        } else if (each.startsWith("-")) {
+            flags[each.slice(1,)] = true
+        } else {
+            numberedArgs.push(each)
+        }
+    }
+    
+    return {flags, namedArgs, numberedArgs}
+}
