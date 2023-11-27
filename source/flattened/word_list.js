728a6641d9bd232d4b12884d5744b8fff46df411
Jeff Hykin <jeff.hykin@gmail.com>
2023-11-27 14:15:07 -0600

    isolate wordList

---
+++ source/flattened/word_list.js
@@ -0,0 +1,5 @@
+export const wordList = (str) => {
+    const addedSeperator = str.replace(/([a-z0-9])([A-Z])/g, "$1_$2").replace(/[^a-zA-Z0-9 _.-]/,"_").toLowerCase()
+    const words = addedSeperator.split(/[ _.-]+/g).filter(each=>each)
+    return words
+}
