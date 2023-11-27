85724a202984870bbc734f095c497af024fc6be9
Jeff Hykin <jeff.hykin@gmail.com>
2023-11-27 15:20:30 -0600

    -

--- source/test/parse_args.js
+++ source/test/parse_args.js
@@ -1,4 +1,4 @@
-import { parseArgs } from "../flattened/parse_args.js"
+import { parseArgs, flag, required } from "../flattened/parse_args.js"
 
 const output = parseArgs({
     rawArgs: [ "1", "2", "--option", "3", ],
@@ -11,3 +11,22 @@ const output = parseArgs({
 console.debug(`output is:`,output)
 console.debug(`output.numberedArgs.length is:`,output.numberedArgs.length)
 console.debug(`output.numberedArgs is:`,output.numberedArgs)
+
+{
+    try {
+        const output = parseArgs({
+            rawArgs: [ "1", "2", "3", ],
+            fields: [
+                [["-o", "--option"], flag, required ],
+                [["--version"], flag ],
+                [["--number"], (value)=>parseInt(value)],
+            ],
+        })
+        console.debug(`output is:`,output)
+        console.debug(`output.numberedArgs.length is:`,output.numberedArgs.length)
+        console.debug(`output.numberedArgs is:`,output.numberedArgs)
+    } catch (error) {
+        console.debug(`THERE SHOULD BE AN ERROR:`,error)
+    }
+    
+}
