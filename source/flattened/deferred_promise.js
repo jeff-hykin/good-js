de9f59a51b42b4d2cd70c078a8f0b19fb05ca9db
Jeff Hykin <jeff.hykin@gmail.com>
2024-03-26 11:32:51 -0500

    make resolve synchonous, push errors into promise

--- source/flattened/deferred_promise.js
+++ source/flattened/deferred_promise.js
@@ -1,15 +1,20 @@
 // https://deno.land/std@0.161.0/async/deferred.ts
-export function deferred() {
+export function deferredPromise() {
     let methods
     let state = "pending"
     const promise = new Promise((resolve, reject) => {
         methods = {
-            async resolve(value) {
-                await value
-                state = "fulfilled"
-                resolve(value)
+            resolve(value) {
+                if (value.catch instanceof Function) {
+                    value.catch(reject)
+                }
+                if (value.then instanceof Function) {
+                    value.then(methods.resolve)
+                } else {
+                    state = "fulfilled"
+                    resolve(value)
+                }
             },
-            // deno-lint-ignore no-explicit-any
             reject(reason) {
                 state = "rejected"
                 reject(reason)
