2bdd2f216505a0b5960f205cfce8a8ae7ab9761a
Jeff Hykin <jeff.hykin@gmail.com>
2024-09-23 15:57:47 -0500

    improve test

--- source/test/after_iterable.js
+++ source/test/after_iterable.js
@@ -2,8 +2,13 @@ import { afterIterable } from "../flattened/after_iterable.js"
 import { iter } from "../flattened/iter.js"
 import { testCallJsonify } from "../flattened/test_call_jsonify.js"
 
-var arg = iter([1, 2, 3])
-var newIterable = afterIterable(arg).then(()=>console.log("iter finished"))
+var newIterable = afterIterable([1, 2, 3]).then(()=>console.log("iter finished"))
+for (const each of newIterable) {
+    console.log(`each is:`,each)
+}
+
+console.log(`empty iterable test`)
+var newIterable = afterIterable([]).then(()=>console.log("iter finished"))
 for (const each of newIterable) {
     console.log(`each is:`,each)
 }
