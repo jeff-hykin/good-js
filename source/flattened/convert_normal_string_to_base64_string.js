6ef8e96def54806e76816672b63c51476998ee49
Jeff Hykin <jeff.hykin@gmail.com>
2024-12-05 11:10:26 -0600

    fix convertNormalStringToBase64String

--- source/flattened/convert_normal_string_to_base64_string.js
+++ source/flattened/convert_normal_string_to_base64_string.js
@@ -1,5 +1,5 @@
-import { convertUint8ArrayToBase64String as bytesToBase64 } from "./convert_uint8_array_to_base64_string.js"
+import { convertUint8ArrayToBase64String } from "./convert_uint8_array_to_base64_string.js"
 
-export function convertNormalStringToBase64String(str, encoder = new TextEncoder()) {
-	return decoder.decode(base64ToBytes(str))
+export function convertNormalStringToBase64String(str, encoder=new TextEncoder()) {
+	return convertUint8ArrayToBase64String(encoder.encode(str))
 }
