928d0df6b9f0230fa5584c0f4f96ef09281e7f0b
Jeff Hykin <jeff.hykin@gmail.com>
2024-12-05 11:18:07 -0600

    fix base64StringToNormalString

--- source/flattened/convert_base64_string_to_normal_string.js
+++ source/flattened/convert_base64_string_to_normal_string.js
@@ -1,5 +1,11 @@
-import { convertBase64StringToUint8Array as bytesToBase64 } from "./convert_base64_string_to_uint8_array.js"
+import { convertBase64StringToUint8Array } from "./convert_base64_string_to_uint8_array.js"
 
-export function base64StringToNormalString(str, encoder = new TextEncoder()) {
-	return bytesToBase64(encoder.encode(str))
+/**
+ * @example
+ * ```js
+ * console.log(base64StringToNormalString("ZWNobyBob3dkeQ=="))
+ * ```
+ */
+export function base64StringToNormalString(str, decoder=new TextDecoder()) {
+	return decoder.decode(convertBase64StringToUint8Array(str))
 }
