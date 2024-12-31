d6059e3549ca87a7a26b63186752be711e3690d3
Jeff Hykin <jeff.hykin@gmail.com>
2024-12-31 10:01:45 -0500

    enhance frequencyCount

--- source/flattened/frequency_count.js
+++ source/flattened/frequency_count.js
@@ -7,6 +7,9 @@ import { isAsyncIterable } from "./is_async_iterable.js"
  * @example
  * ```js
  *     const frequency = frequencyCount([11,11,44,44,9,44,0,0,1,99])
+ *     console.log(frequency.get(11))
+ *     // 2
+ *     console.log(frequency)
  *     // Map {
  *     //     11 => 2,
  *     //     44 => 3,
@@ -15,23 +18,29 @@ import { isAsyncIterable } from "./is_async_iterable.js"
  *     //     1 => 1,
  *     //     99 => 1
  *     // }
+ *     const evenOddCount = frequencyCount([11,11,44,44,9,44,0,0,1,99], { valueToKey: (each)=>each%2 })
+ *     console.log(evenOddCount)
+ *     // Map(2) { 1 => 5, 0 => 5 }
  * ```
  * @returns {Map} output
  *
  */
-export function frequencyCount(iterable) {
+export function frequencyCount(iterable, {valueToKey=null}={}) {
     iterable = makeIterable(iterable)
+    valueToKey = valueToKey || ((each)=>each)
     if (isAsyncIterable(iterable)) {
         return (async function(){
             const counts = new Map()
-            for await (const element of iterable) {
+            for await (let element of iterable) {
+                element = valueToKey(element)
                 counts.set(element, (counts.get(element)||0)+1)
             }
             return counts
         })()
     } else {
         const counts = new Map()
-        for (const element of iterable) {
+        for (let element of iterable) {
+            element = valueToKey(element)
             counts.set(element, (counts.get(element)||0)+1)
         }
         return counts
