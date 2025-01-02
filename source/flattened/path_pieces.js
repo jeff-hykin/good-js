47537d770eaf141205b84e0e53f2223c8aa9b162
Jeff Hykin <jeff.hykin@gmail.com>
2025-01-02 10:27:55 -0500

    add docs

--- source/flattened/path_pieces.js
+++ source/flattened/path_pieces.js
@@ -1,6 +1,12 @@
 import { pathPiecesPosix } from "./path_pieces_posix.js"
 import { pathPiecesWindows } from "./path_pieces_windows.js"
 
+/**
+ * @example
+ * ```js
+ * const [ folders, itemName, itemExtensionWithDot ] = pathPiecesPosix(`/Users/me/Documents/myFolder/myFile.txt`)
+ * ```
+ */
 export function pathPieces(path, { fsType="posix" } = {}) {
     if (fsType == "posix") {
         return pathPiecesPosix(path)
