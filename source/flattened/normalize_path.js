357296ab6445fabccafcc48d439f5e4dec97f305
Jeff Hykin <jeff.hykin@gmail.com>
2025-04-11 14:13:27 -0500

    fix trailing slash

--- source/flattened/normalize_path.js
+++ source/flattened/normalize_path.js
@@ -2,9 +2,14 @@
 import { normalizePathPosix } from "./normalize_path_posix.js"
 import { normalizePathWindows } from "./normalize_path_windows.js"
 
-export function normalizePath(path, { fsType="posix"  }={}) {
+export function normalizePath(path, { fsType="posix", keepTrailingSlash=false, }={}) {
     if (fsType == "posix") {
-        return normalizePathPosix(path)
+        const output = normalizePathPosix(path)
+        if (keepTrailingSlash) {
+            return output
+        }
+        // remove trailing slash
+        return output[output.length-1] == "/" ? output.slice(0, -1) : output
     } else if (fsType == "windows") {
         return normalizePathWindows(path)
     } else {
