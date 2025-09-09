6cf648a266f553799df791b8201bfca67c972924
Jeff Hykin <jeff.hykin@gmail.com>
2025-09-08 19:17:17 -0500

    add support for Uint8Array.fromBase64

--- source/flattened/convert_base64_string_to_uint8_array.js
+++ source/flattened/convert_base64_string_to_uint8_array.js
@@ -8,27 +8,27 @@ import { _getBase64Code as getBase64Code } from "./_get_base64_code.js"
  * console.log(convertBase64StringToUint8Array("ZWNobyBob3dkeQ=="))
  * ```
  */
-export function convertBase64StringToUint8Array(str) {
-	if (str.length % 4 !== 0) {
-		throw new Error("Unable to parse base64 string.");
-	}
-	const index = str.indexOf("=");
-	if (index !== -1 && index < str.length - 2) {
-		throw new Error("Unable to parse base64 string.");
-	}
-	let missingOctets = str.endsWith("==") ? 2 : str.endsWith("=") ? 1 : 0,
-		n = str.length,
-		result = new Uint8Array(3 * (n / 4)),
-		buffer;
-	for (let i = 0, j = 0; i < n; i += 4, j += 3) {
-		buffer =
-			getBase64Code(str.charCodeAt(i)) << 18 |
-			getBase64Code(str.charCodeAt(i + 1)) << 12 |
-			getBase64Code(str.charCodeAt(i + 2)) << 6 |
-			getBase64Code(str.charCodeAt(i + 3));
-		result[j] = buffer >> 16;
-		result[j + 1] = (buffer >> 8) & 0xFF;
-		result[j + 2] = buffer & 0xFF;
-	}
-	return result.subarray(0, result.length - missingOctets);
+export const convertBase64StringToUint8Array = Uint8Array.fromBase64 || function (str) {
+    if (str.length % 4 !== 0) {
+        throw new Error("Unable to parse base64 string.");
+    }
+    const index = str.indexOf("=");
+    if (index !== -1 && index < str.length - 2) {
+        throw new Error("Unable to parse base64 string.");
+    }
+    let missingOctets = str.endsWith("==") ? 2 : str.endsWith("=") ? 1 : 0,
+        n = str.length,
+        result = new Uint8Array(3 * (n / 4)),
+        buffer;
+    for (let i = 0, j = 0; i < n; i += 4, j += 3) {
+        buffer =
+            getBase64Code(str.charCodeAt(i)) << 18 |
+            getBase64Code(str.charCodeAt(i + 1)) << 12 |
+            getBase64Code(str.charCodeAt(i + 2)) << 6 |
+            getBase64Code(str.charCodeAt(i + 3));
+        result[j] = buffer >> 16;
+        result[j + 1] = (buffer >> 8) & 0xFF;
+        result[j + 2] = buffer & 0xFF;
+    }
+    return result.subarray(0, result.length - missingOctets);
 }
