f5e1ea7f433337faa9f25ab2a9cc85e72374cc43
Jeff Hykin <jeff.hykin@gmail.com>
2025-09-08 19:20:08 -0500

    -

--- source/flattened/convert_base64_string_to_normal_string.js
+++ source/flattened/convert_base64_string_to_normal_string.js
@@ -6,6 +6,6 @@ import { convertBase64StringToUint8Array } from "./convert_base64_string_to_uint
  * console.log(base64StringToNormalString("ZWNobyBob3dkeQ=="))
  * ```
  */
-export function base64StringToNormalString(str, decoder=new TextDecoder()) {
-	return decoder.decode(convertBase64StringToUint8Array(str))
+export function base64StringToNormalString(str, decoder = new TextDecoder()) {
+    return decoder.decode(convertBase64StringToUint8Array(str))
 }
--- source/flattened/convert_normal_string_to_base64_string.js
+++ source/flattened/convert_normal_string_to_base64_string.js
@@ -6,6 +6,6 @@ import { convertUint8ArrayToBase64String } from "./convert_uint8_array_to_base64
  * console.log(convertNormalStringToBase64String("echo howdy"))
  * ```
  */
-export function convertNormalStringToBase64String(str, encoder=new TextEncoder()) {
-	return convertUint8ArrayToBase64String(encoder.encode(str))
+export function convertNormalStringToBase64String(str, encoder = new TextEncoder()) {
+    return convertUint8ArrayToBase64String(encoder.encode(str))
 }
